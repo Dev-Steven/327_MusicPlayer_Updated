1e6b10681e0fee74f25d56e2e3a93627ddb76007
Steven <steven.chung96@yahoo.com>
2019-10-18 01:22:03 -0700

    login service works

--- BestMusicPlayer/src/main/java/bestmusicplayer/Login.java
+++ BestMusicPlayer/src/main/java/bestmusicplayer/Login.java
@@ -1,11 +1,18 @@
 package bestmusicplayer;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.LinkedTreeMap;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import javax.swing.JOptionPane;
 import java.io.FileWriter;
 import java.io.IOException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.*; 
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -23,25 +30,58 @@ public class Login {
     String email = "";
     String password = "";
     
-    public void logging_in(String email, String password, javax.swing.JPanel main, javax.swing.JPanel test)
+    JSONArray accountList = new JSONArray();
+    
+    Gson gson = new Gson();
+    GsonBuilder builder = new GsonBuilder();
+    
+    Object obj = new Object();
+    
+    boolean success = false;
+    
+    public void logging_in(String email, String password, javax.swing.JPanel main, javax.swing.JPanel test) 
     {
-        if(email.equals("email") && password.equals("password"))
-        {
-            // Alerts user
-            JOptionPane.showMessageDialog(null, "Login Successful");
+        
+        System.out.println("Entered Email: " + email);
+        System.out.println("Entered Password: " + password);
+        
+        //  reading file
+        try
+            {
+                BufferedReader br = new BufferedReader(  
+                new FileReader("accounts.json"));
+                System.out.print("\nReading the file...\n");
+                accountList = gson.fromJson(br, JSONArray.class);
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        
+        // parsing
+        
+        for (int i = 0; i < accountList.size(); i++) {
+            Object account = accountList.get(i);
+            JSONObject object = new Gson().fromJson(new Gson().toJson(((LinkedTreeMap<String, Object>) account)), JSONObject.class);
             
-            //  use email and password to search for the correct information
-            //  on the JSON file then grab proper data
+            String getEmail = (String)object.get("email");
+            System.out.println("Email: " + getEmail);
+            String getPassword = (String)object.get("_password");
+            System.out.println("Password: " + getPassword);
             
-            //  first account
+            if(email.equals(getEmail) && password.equals(getPassword))
+            {
+                System.out.println("Niiiicee!");
+                success = true;
+                
+                main.removeAll();
+                main.add(test);
+                main.repaint();
+                main.revalidate();
+            }
             
-            // Using card layout, switches panels when Login successful
-            main.removeAll();
-            main.add(test);
-            main.repaint();
-            main.revalidate();
         }
-        else
+        if(!success)
         {
             JOptionPane.showMessageDialog(null, "Login Failure");
         }
